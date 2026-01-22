68868c285363e06fc8f07523f6a6422e46f188e7
Kevin <k120575@gmail.com>
2026-01-22 23:53:54 +0800

    fix tailwindcss not import

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 // vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite' // 1. 引入 Tailwind 插件
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(), // 2. 將它加入插件陣列中
+  ],
   base: '/kevin-portfolio/',
 })
