1bdc6a478049bc080cf6edb14d6a82c09784fb50
Kevin <k120575@gmail.com>
2026-01-22 23:18:09 +0800

    update vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,8 @@
+// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // 1. 引入這個
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // 2. 加入這個插件
-  ],
+  plugins: [react()],
+  base: '/kevin-portfolio/',
 })
